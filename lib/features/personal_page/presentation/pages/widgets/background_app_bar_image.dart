42b492c313fe9b38388825991948308b9b76e9eb
AmrMohamed97 <amr.elzorkany97@gmail.com>
2024-05-30 16:16:21 +0300

    edit personal page

--- lib/features/personal_page/presentation/pages/widgets/background_app_bar_image.dart
+++ lib/features/personal_page/presentation/pages/widgets/background_app_bar_image.dart
@@ -2,41 +2,56 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:note_app/features/personal_page/presentation/manager/background_image_cubit/background_image_cubit.dart';
 import 'package:note_app/features/personal_page/presentation/manager/background_image_cubit/background_image_state.dart';
- import 'package:note_app/features/personal_page/presentation/pages/widgets/change_image_page.dart';
+import 'package:note_app/features/personal_page/presentation/pages/widgets/change_image_page.dart';
 
 class BackgroundAppBarImage extends StatelessWidget {
   const BackgroundAppBarImage({
     super.key,
-   });
+  });
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(create: (context)=>BackgroundImageCubit()..getBackgroundImage(),
-    child: BlocBuilder<BackgroundImageCubit,BackgroundImageState>(
-      builder: (context,state){
-        var cubit = BlocProvider.of<BackgroundImageCubit>(context);
-        return GestureDetector(
-      onTap: () {
-        Navigator.push(
-            context,
-            MaterialPageRoute(
-                builder: (context) => ChangeImagePage(
-                    personalImage: cubit.backgroundImage,
-                    image: 'assets/images/4.png',
-                    folder: 'background')));
-      },
-      child: cubit.backgroundImage.isEmpty
-          ? Image.asset(
-              'assets/images/4.png',
-            )
-          : Image.network(
-              cubit.backgroundImage,
-              fit: BoxFit.cover,
-              width: double.infinity,
-            ),
+    return BlocProvider(
+      create: (context) => BackgroundImageCubit()..getBackgroundImage(),
+      child: BlocBuilder<BackgroundImageCubit, BackgroundImageState>(
+        builder: (context, state) {
+          var cubit = BlocProvider.of<BackgroundImageCubit>(context);
+          return GestureDetector(
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => ChangeImagePage(
+                          personalImage: cubit.backgroundImage,
+                          image: 'assets/images/4.png',
+                          folder: 'background')));
+            },
+            child: cubit.backgroundImage.isEmpty
+                ? Stack(
+                    children: [
+                      Image.asset(
+                        'assets/images/4.png',
+                      ),
+                      const Positioned(
+                        bottom: 20,
+                        left: 20,
+                        child: const CircleAvatar(
+                            radius: 14,
+                            child: Icon(
+                              Icons.camera_alt_rounded,
+                              color: Colors.black,
+                              size: 16,
+                            )),
+                      )
+                    ],
+                  )
+                : Image.network(
+                    cubit.backgroundImage,
+                    fit: BoxFit.cover,
+                    width: double.infinity,
+                  ),
+          );
+        },
+      ),
     );
-      },
-    ),
-    );
-    
   }
 }
--- lib/features/personal_page/presentation/pages/widgets/personal_image.dart
+++ lib/features/personal_page/presentation/pages/widgets/personal_image.dart
@@ -12,13 +12,43 @@ class PersonalImage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return cubit.personalImage.isEmpty
-        ? const CircleAvatar(
-            radius: 35,
-            backgroundImage: AssetImage('assets/images/3.png'),
-          )
-        : CircleAvatar(
-            radius: 35,
-            backgroundImage: NetworkImage(cubit.personalImage),
+        ? const Stack(
+          children: [
+              CircleAvatar(
+                radius: 38,
+                backgroundColor: Colors.white,
+                child: CircleAvatar(
+                  radius: 35,
+                  backgroundImage: AssetImage('assets/images/3.png'),
+                ),
+              ),
+                CircleAvatar(
+                  radius: 10,
+                  child: Icon(
+                    Icons.camera_alt_rounded,
+                    color: Colors.black,
+                    size: 13,
+                  )),
+          ],
+        )
+        : Stack(
+            children: [
+              CircleAvatar(
+                backgroundColor: Colors.white,
+                radius: 38,
+                child: CircleAvatar(
+                  radius: 35,
+                  backgroundImage: NetworkImage(cubit.personalImage),
+                ),
+              ),
+              const CircleAvatar(
+                  radius: 10,
+                  child: Icon(
+                    Icons.camera_alt_rounded,
+                    color: Colors.black,
+                    size: 13,
+                  )),
+            ],
           );
   }
 }
