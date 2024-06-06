36c05665f62efac1a4c32d6c3049c8d5c581b5d1
AmrMohamed97 <amr.elzorkany97@gmail.com>
2024-06-06 16:37:14 +0300

    add notification part two

--- lib/features/tasks/presentation/view/widgets/add_task_method.dart
+++ lib/features/tasks/presentation/view/widgets/add_task_method.dart
@@ -2,12 +2,10 @@ import 'dart:async';
 
 import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:flutter/material.dart';
-import 'package:note_app/core/constants/constances.dart';
-import 'package:note_app/core/utiles/functions/set_up.dart';
-import 'package:note_app/core/widgets/local_notification_services.dart';
-import 'package:note_app/features/tasks/data/model/task_model.dart';
+ import 'package:note_app/features/tasks/data/model/task_model.dart';
 import 'package:note_app/features/tasks/presentation/manager/add_task_cubit/add_task_cubit.dart';
 import 'package:note_app/features/tasks/presentation/manager/task_cubit/task_cubit.dart';
+import 'package:note_app/features/tasks/presentation/view/widgets/check_task_input_validate.dart';
  import 'package:timezone/timezone.dart' as tz;
 
 Future<void> addTaskMethod(
@@ -34,61 +32,7 @@ Future<void> addTaskMethod(
 
     if (taskCubit.taskNameController.text.isNotEmpty ||
         taskCubit.taskContentController.text.isNotEmpty) {
-      if (taskCubit.startDate != null &&
-          taskCubit.endDate != null &&
-          taskCubit.startTime != null &&
-          taskCubit.endTime != null) {
-        var startDate = tz.TZDateTime(
-          tz.local,
-          taskCubit.startDate!.year,
-          taskCubit.startDate!.month,
-          taskCubit.startDate!.day,
-          taskCubit.startTime!.hour,
-          taskCubit.startTime!.minute,
-        );
-        var endDate = tz.TZDateTime(
-          tz.local,
-          taskCubit.endDate!.year,
-          taskCubit.endDate!.month,
-          taskCubit.endDate!.day,
-          taskCubit.endTime!.hour,
-          taskCubit.endTime!.minute,
-        );
-        if (currentDate.isBefore(startDate) && startDate.isBefore(endDate)) {
-          cubit.addTask(task);
-          //------add Notifications-------
-             await getIt.get<LocalNotificationServices>().sendScheduledNotification(
-      id: id,
-      title: 'Task start date',
-      body: 'you should start: ${taskCubit.taskNameController.text.isEmpty?taskCubit.taskContentController.text:taskCubit.taskNameController.text}',
-      scheduledDate: startDate,
-    );
-    await getIt.get<LocalNotificationServices>().sendScheduledNotification(
-      id: id + 1,
-      title: 'task deadline',
-      body:'You must be finished: ${taskCubit.taskNameController.text.isEmpty?taskCubit.taskContentController.text:taskCubit.taskNameController.text}',
-      scheduledDate: endDate,
-    );
-         
-
-        } else {
-          AwesomeDialog(
-            context: context,
-            title: 'error',
-            body: const Text('enter correct date'),
-          ).show();
-          taskCubit.changeLoadingState(state: false);
-          return;
-        }
-      } else {
-        AwesomeDialog(
-          context: context,
-          title: 'error',
-          body: const Text('add   date and   time'),
-        ).show();
-        taskCubit.changeLoadingState(state: false);
-        return;
-      }
+      await checkTaskInputValidate(taskCubit, currentDate, cubit, task, context);
     } else {
       AwesomeDialog(
         context: context,
@@ -96,8 +40,6 @@ Future<void> addTaskMethod(
         body: const Text('add task name or content'),
       ).show();
       taskCubit.changeLoadingState(state: false);
-      return;
-    }
-  
-  
+     }
 }
+
---
+++ lib/features/tasks/presentation/view/widgets/check_task_input_validate.dart
@@ -0,0 +1,65 @@
+import 'package:awesome_dialog/awesome_dialog.dart';
+import 'package:flutter/material.dart';
+import 'package:note_app/core/constants/constances.dart';
+import 'package:note_app/core/utiles/functions/set_up.dart';
+import 'package:note_app/core/widgets/local_notification_services.dart';
+import 'package:note_app/features/tasks/data/model/task_model.dart';
+import 'package:note_app/features/tasks/presentation/manager/add_task_cubit/add_task_cubit.dart';
+import 'package:note_app/features/tasks/presentation/manager/task_cubit/task_cubit.dart';
+ import 'package:timezone/timezone.dart' as tz;
+
+Future<void> checkTaskInputValidate(TaskCubit taskCubit, tz.TZDateTime currentDate, AddTaskCubit cubit, TaskModel task, BuildContext context) async {
+  if (taskCubit.startDate != null &&
+      taskCubit.endDate != null &&
+      taskCubit.startTime != null &&
+      taskCubit.endTime != null) {
+    var startDate = tz.TZDateTime(
+      tz.local,
+      taskCubit.startDate!.year,
+      taskCubit.startDate!.month,
+      taskCubit.startDate!.day,
+      taskCubit.startTime!.hour,
+      taskCubit.startTime!.minute,
+    );
+    var endDate = tz.TZDateTime(
+      tz.local,
+      taskCubit.endDate!.year,
+      taskCubit.endDate!.month,
+      taskCubit.endDate!.day,
+      taskCubit.endTime!.hour,
+      taskCubit.endTime!.minute,
+    );
+    if (currentDate.isBefore(startDate) && startDate.isBefore(endDate)) {
+      cubit.addTask(task);
+      //------add Notifications-------
+         await getIt.get<LocalNotificationServices>().sendScheduledNotification(
+  id: id,
+  title: 'Task start date',
+  body: 'you should start: ${taskCubit.taskNameController.text.isEmpty?taskCubit.taskContentController.text:taskCubit.taskNameController.text}',
+  scheduledDate: startDate,
+      );
+      await getIt.get<LocalNotificationServices>().sendScheduledNotification(
+  id: id + 1,
+  title: 'task deadline',
+  body:'You must be finished: ${taskCubit.taskNameController.text.isEmpty?taskCubit.taskContentController.text:taskCubit.taskNameController.text}',
+  scheduledDate: endDate,
+      );
+     
+  
+    } else {
+      AwesomeDialog(
+        context: context,
+        title: 'error',
+        body: const Text('enter correct date'),
+      ).show();
+      taskCubit.changeLoadingState(state: false);
+     }
+  } else {
+    AwesomeDialog(
+      context: context,
+      title: 'error',
+      body: const Text('add   date and   time'),
+    ).show();
+    taskCubit.changeLoadingState(state: false);
+   }
+}
