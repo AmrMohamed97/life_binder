ef9e70efaca68dfe2b237e8d3395b9e3a9186b0e
AmrMohamed97 <amr.elzorkany97@gmail.com>
2024-06-09 06:59:19 +0300

    add whatsapp and phone call

--- lib/features/personal_page/presentation/pages/widgets/custom_drawer.dart
+++ lib/features/personal_page/presentation/pages/widgets/custom_drawer.dart
@@ -1,12 +1,13 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
- import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:note_app/core/constants/colors/app_colors.dart';
- import 'package:note_app/core/routes/pages_keys.dart';
+import 'package:note_app/core/routes/pages_keys.dart';
 import 'package:note_app/core/utiles/functions/sign_out.dart';
 import 'package:note_app/features/personal_page/presentation/manager/person_info_cubit/person_info_cubit.dart';
 import 'package:note_app/features/tasks/presentation/view/task_home_page.dart';
- 
+import 'package:url_launcher/url_launcher.dart';
+
 class CustomDrawer extends StatelessWidget {
   const CustomDrawer({
     super.key,
@@ -54,24 +55,50 @@ class CustomDrawer extends StatelessWidget {
                   title: const Text('add note'),
                 ),
                 ListTile(
-                  onTap: ()  {
-                   Navigator.of(context)
-                        .pushReplacement(MaterialPageRoute(builder: (context) => TaskHmoePage(personalImage: cubit.personalImage,userName: cubit.userName,) ));
+                  onTap: () {
+                    Navigator.of(context).pushReplacement(MaterialPageRoute(
+                        builder: (context) => TaskHmoePage(
+                              personalImage: cubit.personalImage,
+                              userName: cubit.userName,
+                            )));
                   },
                   leading: const Icon(Icons.add_task),
                   title: const Text('add task'),
                 ),
                 ListTile(
-                  onTap: ()   {
-                    Navigator.of(context)
-                        .pushReplacement(MaterialPageRoute(builder: (context) => TaskHmoePage(page: 1,personalImage: cubit.personalImage,userName: cubit.userName,) ));
+                  onTap: () {
+                    Navigator.of(context).pushReplacement(MaterialPageRoute(
+                        builder: (context) => TaskHmoePage(
+                              page: 1,
+                              personalImage: cubit.personalImage,
+                              userName: cubit.userName,
+                            )));
                   },
                   leading: const Icon(Icons.task_alt),
                   title: const Text('Tasks Page'),
                 ),
-                const ListTile(
-                  title:   Text('whatsapp'),
-                  leading:  FaIcon(FontAwesomeIcons.whatsapp),
+                ListTile(
+                  title: const Text('Whatsapp'),
+                  leading: const Icon(FontAwesomeIcons.whatsapp),
+                  onTap: () async {
+                    Uri uri = Uri.parse('https://wa.me/?text=?');
+                    if (await canLaunchUrl(uri)) {
+                      await launchUrl(uri);
+                    }
+                  },
+                ),
+                ListTile(
+                  title: const Text('Call'),
+                  leading: const Icon(
+                    FontAwesomeIcons.phone,
+                    size: 20,
+                  ),
+                  onTap: () async {
+                    Uri uri = Uri.parse('tel:');
+                    if (await canLaunchUrl(uri)) {
+                      await launchUrl(uri);
+                    }
+                  },
                 ),
                 const Expanded(
                     child: SizedBox(
