8264b27dea202313169eed19b56ba9f2c970c5d5
AmrMohamed97 <amr.elzorkany97@gmail.com>
2024-06-11 15:59:50 +0300

    edit done tasks

--- lib/features/tasks/presentation/view/Done_tasks.dart
+++ lib/features/tasks/presentation/view/Done_tasks.dart
@@ -1,9 +1,10 @@
+  
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:note_app/features/tasks/presentation/manager/fetch_done_tasks_cubit/fetch_done_task_cubit.dart';
 import 'package:note_app/features/tasks/presentation/manager/fetch_done_tasks_cubit/fetch_done_task_state.dart';
- import 'package:note_app/features/tasks/presentation/view/widgets/all_tasks_app_bar.dart';
- import 'package:note_app/features/tasks/presentation/view/widgets/done_tasks_list_view.dart';
-import 'package:flutter/material.dart';
+import 'package:note_app/features/tasks/presentation/view/widgets/all_tasks_app_bar.dart';
+import 'package:note_app/features/tasks/presentation/view/widgets/done_tasks_list_view.dart';
 
 class DoneTasks extends StatelessWidget {
   const DoneTasks({super.key});
