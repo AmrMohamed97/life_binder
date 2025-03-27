ab500e77d7d3f33694f5e09a1be2b801fe2d9b29
amrMohamed-pureSoft <amr.mohamed@pure-soft.com>
2025-03-27 18:00:43 +0200

    update part2

--- lib/core/widgets/work_manager_service.dart
+++ lib/core/widgets/work_manager_service.dart
@@ -1,54 +1,54 @@
-import 'package:note_app/core/utiles/functions/set_up.dart';
-import 'package:note_app/core/widgets/local_notification_services.dart';
-import 'package:workmanager/workmanager.dart';
- import 'package:timezone/timezone.dart' as tz;
+// import 'package:note_app/core/utiles/functions/set_up.dart';
+// import 'package:note_app/core/widgets/local_notification_services.dart';
+// import 'package:workmanager/workmanager.dart';
+//  import 'package:timezone/timezone.dart' as tz;
 
-class WorkManagerService {
-  Future<void> init() async {
-    await Workmanager().initialize(
-      action,
-      isInDebugMode: true,
-    ); //Before registering any task, the WorkManager plugin must be initialized.
-    //  registerOneTask();
-    await registerPeriodicTask();
-  }
+// class WorkManagerService {
+//   Future<void> init() async {
+//     await Workmanager().initialize(
+//       action,
+//       isInDebugMode: true,
+//     ); //Before registering any task, the WorkManager plugin must be initialized.
+//     //  registerOneTask();
+//     await registerPeriodicTask();
+//   }
 
-  Future<void> registerOneTask() async {
-    await Workmanager().registerOneOffTask(
-        'task-identifier' /*to be able to do collection of tasks*/,
-        'simplePeriodicTask' /*will be sent to dispatcher to  indicating the task's type. */);
-  }
+//   Future<void> registerOneTask() async {
+//     await Workmanager().registerOneOffTask(
+//         'task-identifier' /*to be able to do collection of tasks*/,
+//         'simplePeriodicTask' /*will be sent to dispatcher to  indicating the task's type. */);
+//   }
 
-  Future<void> registerPeriodicTask() async {
-    await Workmanager().registerPeriodicTask(
-      'periodicId-cc',
-      'periodicTask',
-      frequency: const Duration(hours: 24),
-    );
-  }
+//   Future<void> registerPeriodicTask() async {
+//     await Workmanager().registerPeriodicTask(
+//       'periodicId-cc',
+//       'periodicTask',
+//       frequency: const Duration(hours: 24),
+//     );
+//   }
 
-  void cancleTask(String id) {
-    Workmanager().cancelByUniqueName(id);
-  }
-}
+//   void cancleTask(String id) {
+//     Workmanager().cancelByUniqueName(id);
+//   }
+// }
 
-@pragma('vm:entry-point')
-void action() {
-  //The callbackDispatcher needs to be either a static function or a top level function to be accessible as a Flutter entry point.
-  Workmanager().executeTask((task, inputData) {
-    getIt.get<LocalNotificationServices>().sendScheduledNotification(
-      id: 0,
-      title: 'Time_waver',
-      body: 'You can use the app to schedule your day to make the most of it',
-      payload: 'daily',
-      scheduledDate: tz.TZDateTime(
-      tz.local,
-      DateTime.now().year,
-      DateTime.now().month,
-      DateTime.now().day,
-      9,
-    ),
-    );
-    return Future.value(true);
-  });
-}
+// @pragma('vm:entry-point')
+// void action() {
+//   //The callbackDispatcher needs to be either a static function or a top level function to be accessible as a Flutter entry point.
+//   Workmanager().executeTask((task, inputData) {
+//     getIt.get<LocalNotificationServices>().sendScheduledNotification(
+//       id: 0,
+//       title: 'Time_waver',
+//       body: 'You can use the app to schedule your day to make the most of it',
+//       payload: 'daily',
+//       scheduledDate: tz.TZDateTime(
+//       tz.local,
+//       DateTime.now().year,
+//       DateTime.now().month,
+//       DateTime.now().day,
+//       9,
+//     ),
+//     );
+//     return Future.value(true);
+//   });
+// }
--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,11 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_timezone/flutter_timezone.dart';
+// import 'package:flutter_timezone/flutter_timezone.dart';
 import 'package:note_app/core/utiles/cache_helper.dart';
 import 'package:note_app/core/utiles/functions/set_up.dart';
 import 'package:note_app/core/widgets/local_notification_services.dart';
-import 'package:note_app/core/widgets/work_manager_service.dart';
+// import 'package:note_app/core/widgets/work_manager_service.dart';
 import 'package:note_app/features/add_note/presentation/manager/add_note_cubit.dart';
 import 'package:note_app/features/auth/presentation/manager/app_theme_cubit/app_theme_cubit.dart';
 import 'package:note_app/features/auth/presentation/manager/app_theme_cubit/app_theme_state.dart';
@@ -20,7 +20,7 @@ import 'core/constants/constances.dart';
 import 'core/utiles/my_observer/my_observer.dart';
 import 'features/edit_note/presentation/manager/edit_note_cubit.dart';
 import 'package:timezone/data/latest_all.dart' as tz;
-import 'package:timezone/timezone.dart' as tz;
+// import 'package:timezone/timezone.dart' as tz;
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -36,15 +36,15 @@ Future<void> main() async {
   await getIt.get<CacheHelper>().init();
 
   tz.initializeTimeZones();
-  final String currentTimeZone = await FlutterTimezone.getLocalTimezone();
-  tz.setLocalLocation(tz.getLocation(currentTimeZone));
+  // final String currentTimeZone = await FlutterTimezone.getLocalTimezone();
+  // tz.setLocalLocation(tz.getLocation(currentTimeZone));
   debugPrint('================allow notification appear=================');
   await FirebaseMessaging.instance.requestPermission();
   debugPrint('===============initialize local notification==============');
   await getIt.get<LocalNotificationServices>().initialize();
   getIt.get<CacheHelper>().saveData(key: 'notificationId', value: 1);
 
-  await WorkManagerService().init();
+  // await WorkManagerService().init();
   Bloc.observer = MyObserver();
   runApp(const LifeBinder());
 }
